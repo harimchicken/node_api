f822b112e26f252efb59f3317290889285967893
leeharim <idegami1559@icloud.com>
2021-08-12 12:54:17 +0900

    add order model

---
+++ models/order.js
@@ -0,0 +1,50 @@
+// 1
+const mongoose = require('mongoose')
+
+
+
+
+// 2
+const orderSchema = mongoose.Schema(
+    {
+        product: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'product',
+            required: true
+        },
+        quantity: {
+            type: Number,
+            default: 1
+        }
+    },
+    {
+        timestamps: true
+    }
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 3
+module.exports = mongoose.model('order', orderSchema)
